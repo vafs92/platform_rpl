8b0cf346520ae4f0816158e9a8cc876321372090
annSolstice <117833595+annSolstice@users.noreply.github.com>
2023-06-01 18:58:43 +0700

    Update index.php

--- Tugas_mvc/app/views/login/index.php
+++ Tugas_mvc/app/views/login/index.php
@@ -1,12 +1,46 @@
-<h3>Login</h3>
-<form action="<?= BASEURL ?>/login/login" method="post">
-    <div class="form-group">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <style>
+        body {
+            background: url('<?= BASEURL; ?> /img/Home.jpg') no-repeat center center fixed;
+            background-size: cover;
+        }
+        .login-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .login-box {
+            background: #fff;
+            padding: 20px;
+            max-width: 400px;
+            width: 100%;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-box">
+            <h3>Login</h3>
+            <form action="<?= BASEURL ?>/login/login" method="post">
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" class="form-control" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
     </div>
-    <div class="form-group">
-        <label for="password">Password:</label>
-        <input type="text" id="password" name="password" required>
-    </div>
-    <button type="submit">Login</button>
-</form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
