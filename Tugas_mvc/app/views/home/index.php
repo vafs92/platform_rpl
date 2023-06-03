7dd4627dbf6e4c2d8461885b82588ced1e37cd41
loadtriani <126430409+loadtriani@users.noreply.github.com>
2023-06-03 19:27:44 +0700

    Update index.php

--- Tugas_mvc/app/views/home/index.php
+++ Tugas_mvc/app/views/home/index.php
@@ -1,6 +1,107 @@
-<div class="container">
-<div class="mt-4 p-5 bg-primary text-white rounded">
-  <h1>Selamat Datang</h1>
-</div>
-</div>
+<style>
+  h1{
+    color: cornflowerblue;
+  }
+</style>
 
+
+<!-- Vendor CSS Files -->
+<link href="<?= BASEURL; ?>/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="<?= BASEURL; ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= BASEURL; ?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?= BASEURL; ?>/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?= BASEURL; ?>/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?= BASEURL; ?>/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?= BASEURL; ?>/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?= BASEURL; ?>/assets/css/style.css" rel="stylesheet">
+    
+<head>
+  <body>
+
+  <!-- ======= Label ======= -->
+  <header id="header" class="fixed-down d-flex align-items-center">
+  <div class="container d-flex justify-content-center justify-content-md-between">
+    <div class="container d-flex align-items-center justify-content-between">
+    
+      <div class="logo">
+        <h1 style color>Sistem Informasi Peminjaman Barang & Ruang</h1>
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+    </div>
+    <div id="topbar" class="fixed-down d-flex align-items-center">
+      <div class="social-links d-none d-md-flex">
+          <a href="https://www.usd.ac.id/fakultas/sainsdanteknologi/" class="facebook"><i class="bi bi-globe"></i></a>
+          <a href="https://www.instagram.com/bemfst_usd/" class="instagram"><i class="bi bi-instagram"></i></a>
+          <a href="https://www.youtube.com/@kpufstusd5012" class="youtube"><i class="bi bi-youtube"></i></i></a>
+      </div>
+    </div>
+  </div>
+  </header><!-- End Label -->
+  
+  <section id="hero" style="margin-top: 0px;">
+    <div class="hero-container" >
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner">
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url(<?= BASEURL; ?>/img/HomeUP.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate_animated animate_fadeInDown">Selamat Datang di <span>SIP</span></h2>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 2 -->
+          <div class="carousel-item" style="background-image: url(<?= BASEURL; ?>/img/gedungFST.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate_animated animate_fadeInDown">Fakultas Sains dan Teknologi</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+        <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon ri-arrow-left-line" aria-hidden="true"></span>
+        </a>
+
+        <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon ri-arrow-right-line" aria-hidden="true"></span>
+        </a>
+
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <!-- <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+        </div>
+      </div>
+    </div> -->
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Kelompok_3</span></strong>
+      </div>
+      <!-- <div class="credits">
+        Home Designed by <a href="https://bootstrapmade.com/">Bootstrap</a>
+      </div> -->
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  
+    <!-- Template Main JS File -->
+    <script src="<?= BASEURL; ?>/assets/js/main.js"></script>
+  </body>
+</head>
