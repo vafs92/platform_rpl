bce28ef75d8865f47164b77565974286f00f32eb
vafs92 <126405181+vafs92@users.noreply.github.com>
2023-06-03 22:42:51 +0700

    Update Model_User.php

--- Tugas_mvc/app/models/Model_User.php
+++ Tugas_mvc/app/models/Model_User.php
@@ -14,4 +14,13 @@ class Model_User {
         $this->db->bind(':username', $username);
         return $this->db->single();
     }
+    public function getUserByUsernameAndPassword($username, $password) {
+    // Query database untuk mendapatkan data pengguna berdasarkan username dan password
+        $query = "SELECT * FROM table_user WHERE username = :username AND password = :password";
+        $this->db->query($query);
+        $this->db->bind('username', $username);
+        $this->db->bind('password', $password);
+        return $this->db->single();
+    }
+
 }
